22c729f92774cf5977a02583d0437fbfa080be5b
Nicodemos Mateus <nicodemos.mateus@jupiter.co.ao>
2024-05-03 11:59:04 +0100

    refactorar migration doacoes

--- database/migrations/2024_03_05_130853_create_doacoes_table.php
+++ database/migrations/2024_03_05_130853_create_doacoes_table.php
@@ -14,16 +14,20 @@ return new class extends Migration
         Schema::create('doacoes', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('doador_id');
-            $table->unsignedBigInteger('beneficiario_id');
+            $table->unsignedBigInteger('campanha_id')->nullable();
+            $table->unsignedBigInteger('bem_material_id')->nullable();
             $table->decimal('quantia', 10, 2)->nullable()->default(0);
             $table->enum('estado',['Pendente', 'Aceite','Cancelado',"Não Aceite"]);
-            $table->enum('eliminado',[0, 1])->default(0);
+            $table->enum('tipo',['Campanha', 'Bens Materiais']);
             $table->text('descricao')->nullable();
+            $table->enum('eliminado',[0, 1])->default(0);
             $table->timestamps();
 
             $table->foreign('doador_id')->references('id')->on('users')->onUpdate('cascade')
                 ->onDelete('cascade');
-            $table->foreign('beneficiario_id')->references('id')->on('campanhas')->onUpdate('cascade')
+            $table->foreign('campanha_id')->references('id')->on('campanhas')->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreign('bem_material_id')->references('id')->on('doacao_bens_materiais')->onUpdate('cascade')
                 ->onDelete('cascade');
         });
     }
